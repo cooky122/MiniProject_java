8247fc0e9c0b7a44c8dae4b528d29c99fdcf137d
chae seung hyun <snghynchae@naver.com>
2025-06-02 09:04:40 +0900

    주석처리 제거

--- src/dbconnection/Comment/Testing.java
+++
@@ -1,7 +0,0 @@
-package dbconnection.Comment;
-
-//public class Testing {
-//    public static void main(String[] args) {
-//        CommentMain.Start();
-//    }
-//}
--- src/dbconnection/post/Main.java
+++
@@ -1,7 +0,0 @@
-//package dbconnection.post;
-//
-//public class Main {
-//    public static void main(String[] args){
-//        PostMain.start();
-//    }
-//}
